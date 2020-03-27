71600d161ad92dc96dccdb6147bc84d7f144d68a
chenchira9898 <44578889+chenchira9898@users.noreply.github.com>
2020-03-27 22:58:34 +0700

    **

--- saveorder.php
+++ saveorder.php
@@ -19,8 +19,8 @@ echo '<pre>';
 print_r($_POST);
 echo '</pre>';
 
-exit;
 
+//exit;
 
 $name = $_POST["name"];
 $address = $_POST["address"];
@@ -29,15 +29,25 @@ $phone = $_POST["phone"];
 //$total_qty = $_POST["total_qty"];
 $total = $_POST["total"];
 $dttm = Date("Y-m-d G:i:s");
+
+
 //บันทึกการสั่งซื้อลงใน order_detail
-mysqli_query($conn, "BEGIN");
-$sql1    = "insert into order_head values(null, '$dttm', '$name', '$address', '$email', '$phone', '$total_qty', '$total')";
+//mysqli_query($conn, "BEGIN");
+$sql1    = "INSERT INTO order_head 
+VALUES(null, '$dttm', '$name', '$address', '$email', '$phone', '$total')";
 $query1    = mysqli_query($conn, $sql1);
+
+
+
+echo $sql1;
+//exit;
 //ฟังก์ชั่น MAX() จะคืนค่าที่มากที่สุดในคอลัมน์ที่ระบุ ออกมา หรือจะพูดง่ายๆก็ว่า ใช้สำหรับหาค่าที่มากที่สุด นั่นเอง.
 $sql2 = "select max(o_id) as o_id from order_head where o_name='$name' and o_email='$email' and o_dttm='$dttm' ";
 $query2    = mysqli_query($conn, $sql2);
 $row = mysqli_fetch_array($query2);
 $o_id = $row["o_id"];
+
+//exit;
 //PHP foreach() เป็นคำสั่งเพื่อนำข้อมูลออกมาจากตัวแปลที่เป็นประเภท array โดยสามารถเรียกค่าได้ทั้ง $key และ $value ของ array
 foreach ($_SESSION['cart'] as $p_id => $qty) {
     $sql3    = "SELECT * FROM product WHERE p_id=$p_id";
@@ -45,7 +55,7 @@ foreach ($_SESSION['cart'] as $p_id => $qty) {
     $row3    = mysqli_fetch_array($query3);
     $total    = $row3['p_price'] * $qty;
 
-    $sql4    = "insert into order_detail values(null, '$o_id', '$p_id', '$qty', '$total')";
+    $sql4    = "INSERT INTO order_detail VALUES(null, '$o_id', '$p_id', '$qty', '$total')";
     $query4    = mysqli_query($conn, $sql4);
 }
 
